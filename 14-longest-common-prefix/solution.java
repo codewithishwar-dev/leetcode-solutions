69b3a15b32fdafbacf102dc6d17895dfd789d0c6
Code With Ishwar <codewithishwar@gmail.com>
2026-03-15 23:10:46 +0530

    Add solution for LeetCode 14 - Longest Common Prefix

---
+++ 14-longest-common-prefix/solution.java
@@ -0,0 +1,35 @@
+/*
+LeetCode: 14. Longest Common Prefix
+Difficulty: Easy
+
+Approach:
+Vertical Scanning
+
+We compare characters of each string column by column.
+If a mismatch is found, return the prefix formed so far.
+*/
+
+class Solution {
+
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        for (int i = 0; i < strs[0].length(); i++) {
+
+            char currentChar = strs[0].charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+
+                if (i == strs[j].length() || strs[j].charAt(i) != currentChar) {
+                    return strs[0].substring(0, i);
+                }
+
+            }
+        }
+
+        return strs[0];
+    }
+}

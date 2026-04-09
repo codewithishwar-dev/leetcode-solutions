e2254c67e4ec82cc90c2e89143108febcec012cd
Code With Ishwar | Ishwar Chandra Tiwari <codewithishwar@gmail.com>
2026-04-09 13:33:39 +0530

    Implement mergeTwoLists to merge two sorted lists

---
+++ 21-merge-two-sorted-lists/Solution.java
@@ -0,0 +1,31 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
+    }
+}

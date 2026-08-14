b6cb4f9db4975aface049493a4cd2d9ff25d4157
Code With Ishwar | Ishwar Chandra Tiwari <codewithishwar@gmail.com>
2026-08-14 20:44:48 +0530

    Implement maximum length substring function

---
+++ 3090-maximum-length-substring-with-two-occurrences/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        freq = [0] * 26
+
+        left = 0
+        max_length = 0
+
+        for right in range(len(s)):
+            index = ord(s[right]) - ord('a')
+            freq[index] += 1
+
+            while freq[index] > 2:
+                left_index = ord(s[left]) - ord('a')
+                freq[left_index] -= 1
+                left += 1
+
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
